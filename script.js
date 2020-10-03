60b0ec7cd4c8c57df3bf7e399564085ad838b722
alekkss <59772621+alekkss@users.noreply.github.com>
2020-10-03 07:30:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let obj = {
     span: document.querySelector('.burger-menu__span'),
     burgerMenu: document.querySelector('.burger-menu'),
     burgerMenuAction: document.querySelector('.action'),
-    nav: document.querySelector('.nav')
+    nav: document.querySelector('.nav'),
+    close: document.querySelector('.close'),
+    feedback: document.querySelector('.feedback')
 };
 
 let funBurgerMenu = obj.burgerMenu.addEventListener('click', function () {
@@ -15,5 +17,8 @@ let funBurgerMenu = obj.burgerMenu.addEventListener('click', function () {
         obj.burgerMenu.classList.add('action');
         obj.nav.style.display = 'flex';
     }
+});
+obj.close.addEventListener('click', function () {
+    obj.feedback.style.display = 'none';
 });
 
