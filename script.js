cfc1130837c2f431c0074297a79096b1bca85e68
alekkss <59772621+alekkss@users.noreply.github.com>
2020-10-04 16:47:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ let obj = {
     burgerMenuAction: document.querySelector('.action'),
     nav: document.querySelector('.nav'),
     close: document.querySelector('.close'),
-    feedback: document.querySelector('.feedback')
+    feedback: document.querySelector('.feedback'),
+    title: document.querySelector('.header-title'),
+    logo: document.querySelector('.header-logo')
 };
 
 let funBurgerMenu = obj.burgerMenu.addEventListener('click', function () {
     obj.span.style.display = 'none';
     if (obj.burgerMenu.className == "burger-menu action") {
         obj.burgerMenu.classList.remove('action');
+        obj.title.classList.remove('marginOff');
+        obj.logo.classList.remove('displayOff');
+        obj.title.style.margin = 'auto';
         obj.span.style.display = 'flex';
         obj.nav.style.display = 'none';
     } else {
+        obj.title.classList.add('marginOff');
+        obj.logo.classList.add('displayOff');
         obj.burgerMenu.classList.add('action');
         obj.nav.style.display = 'flex';
+        obj.title.style.margin = '100px auto 0';
     }
 });
 obj.close.addEventListener('click', function () {
